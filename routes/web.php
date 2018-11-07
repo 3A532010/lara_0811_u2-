0776a2ee4e19b278efb79b4996ad4188f2cc51b3
3A532010 <s3a532010@student.ncut.edu.tw>
2018-11-07 20:29:17 +0800

    設定Route前置

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,15 @@ Route::get('say/{name?}',['as'=>'hello.index',function($name){
     return'Hello,'.$name;
 
 }]);
-Route::group(['prefix'=>'admin'],function(){
-    Route::get('dashboard',function(){
-        return'dashboard';
+Route::get('dashboard', function () {
+    return 'dashboard';
+});
+Route::group(['prefix'=>'admin'], function () {
+    Route::get('dashboard', function () {
+        return 'admin dashboard';
+
     });
+
 });
 
 
