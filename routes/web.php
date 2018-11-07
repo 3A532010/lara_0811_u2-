d41455d1fce2b2f92cb0085fe0c501cd9ff3ca05
3A532010 <s3a532010@student.ncut.edu.tw>
2018-11-07 20:04:24 +0800

    修改參數成選擇性

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/',function() {
 Route::get('welcome',function() {
     return view('welcome');
 });
-Route::get('hello/{name}',function($name){
+Route::get('hello/{name?}',function($name='Everybody'){
     return'Hello,'.$name;
 
 
